584fac96d9ec8d31c47704d0a7bac7b83e0a17f8
arthuralt <144811366+arthuralt@users.noreply.github.com>
2024-12-19 09:42:56 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/secret-santa/", // add repository name here
+  base: "/secret-santa-main/", // add repository name here
   plugins: [react()],
 });
